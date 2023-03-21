0c7c884c88a236367b9c30ff3b77acb8d4288923
omari <osmith5544@gmail.com>
2023-03-21 16:02:53 -0500

    deleted implementation of app and init classes

--- src/main/java/com/studentmanagement/config/AppConfig.java
+++ src/main/java/com/studentmanagement/config/AppConfig.java
@@ -19,57 +19,7 @@ import javax.sql.DataSource;
 @ComponentScan( basePackages = "com.studentmanagement")
 public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public InternalResourceViewResolver viewResolver(){
-        InternalResourceViewResolver viewResolver
-                = new InternalResourceViewResolver();
 
-        viewResolver.setPrefix("WEB-INF/view/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-
-    @Bean
-    public MessageSource messageSource(){
-        ReloadableResourceBundleMessageSource messageSource
-                = new ReloadableResourceBundleMessageSource();
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setBasename("classpath:messages");
-        return messageSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(){
-        JdbcTemplate jdbcTemplate
-                = new JdbcTemplate(dataSource());
-        return jdbcTemplate;
-    }
-
-    DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/studentmanager?useSSL=false");
-        dataSource.setUsername("root");
-        dataSource.setPassword("mysqlroot1");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        return dataSource;
-    }
-
-//    @Bean
-//    public LocalValidatorFactoryBean validator(){
-//        LocalValidatorFactoryBean validator
-//                = new LocalValidatorFactoryBean();
-//        validator.setValidationMessageSource(messageSource());
-//        return validator;
-//    }
-//
-//    @Override
-//    public Validator getValidator() {
-//        return validator();
-//    }
-
-    public void addFormatters(FormatterRegistry registry){
-
-    }
 
 
 }
